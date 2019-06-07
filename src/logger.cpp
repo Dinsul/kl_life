09b9a7c3051e59d2dc8c8dea981aa1b2eba0f12a
Dinsul <dinsul2.0@gmail.com>
2019-06-07 13:58:59 +0300

    Добавленна система журналирования.
    Добавил класс для журналирования Logger.
    Имеются три вида журналирования:
    1. дописывать в файл
    2. переписывать файл при каждом запуске
    3. в потоки stdout/stderr
    
    Виды журналирования можно комбинировать.
    Настройки для журналирования хранятся в отдельном классе LogSettings.

---
+++ src/logger.cpp
@@ -0,0 +1,186 @@
+#include <spdlog/sinks/daily_file_sink.h>
+#include <spdlog/sinks/stdout_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/spdlog.h>
+#include <boost/filesystem.hpp>
+
+#include "logger.h"
+#include "info.h"
+#include "settings.h"
+
+std::shared_ptr<spdlog::logger> &CGL::Logger::file_log()
+{
+    static std::shared_ptr<spdlog::logger> _fileLogger;
+    if (!_fileLogger)
+    {
+        boost::filesystem::path logPath(LogSettings::get().logPath);
+
+        // Проверяем существование директории
+        // Если такой нет, создаём
+        if (!boost::filesystem::exists(logPath)) {
+            boost::filesystem::create_directories(logPath);
+        }
+
+        logPath.append(cat('.', PRJ_NAME, "log"));
+
+        _fileLogger = spdlog::daily_logger_mt(PRJ_NAME, logPath.string());
+
+        _fileLogger->set_level(static_cast<spdlog::level::level_enum>(LogSettings::get().logLevel));
+
+        // Информацию о запуске пишем всегда
+        _fileLogger->set_pattern("\n--~==# %H:%M:%S.%e %v #==~--");
+        _fileLogger->log(spdlog::level::off, cat(PRJ_NAME, " ", PRJ_VERSION));
+
+        _fileLogger->set_pattern("%H:%M:%S.%e [%l] %v");
+    }
+
+    return _fileLogger;
+}
+
+std::shared_ptr<spdlog::logger> &CGL::Logger::last_log()
+{
+    static std::shared_ptr<spdlog::logger> _lastLogger;
+    if (!_lastLogger)
+    {
+        boost::filesystem::path logPath(LogSettings::get().logPath);
+
+        // Проверяем существование директории
+        // Если такой нет, создаём
+        if (!boost::filesystem::exists(logPath)) {
+            boost::filesystem::create_directories(logPath);
+        }
+
+        logPath.append(cat('.', PRJ_NAME, "last", "log"));
+
+        _lastLogger = spdlog::basic_logger_st(PRJ_NAME"_last", logPath.string(), true);
+
+        // Информацию о запуске пишем всегда
+        _lastLogger->set_pattern("%v run %c\n--------====================--------");
+        _lastLogger->log(spdlog::level::off, cat(PRJ_NAME, ":", PRJ_VERSION));
+
+        _lastLogger->set_pattern("%H:%M:%S.%e [%l] %v");
+
+        _lastLogger->set_level(static_cast<spdlog::level::level_enum>(LogSettings::get().logLevel));
+    }
+
+    return _lastLogger;
+}
+
+std::shared_ptr<spdlog::logger> &CGL::Logger::stdout_log()
+{
+    static std::shared_ptr<spdlog::logger> _constd;
+
+    if (!_constd)
+    {
+        _constd = spdlog::stdout_logger_mt("stdout");
+
+        _constd->set_pattern("%H:%M:%S.%e [%l] %v");
+        _constd->set_level(static_cast<spdlog::level::level_enum>(LogSettings::get().logLevel));
+    }
+
+    return _constd;
+}
+
+std::shared_ptr<spdlog::logger> &CGL::Logger::stderr_log()
+{
+    static std::shared_ptr<spdlog::logger> _conerr;
+
+    if (!_conerr)
+    {
+        _conerr = spdlog::stderr_logger_mt("stderr");
+
+        _conerr->set_pattern("%H:%M:%S.%e [%l] %v");
+        _conerr->set_level(static_cast<spdlog::level::level_enum>(LogSettings::get().logLevel));
+    }
+
+    return _conerr;
+}
+
+void CGL::Logger::trace(const std::string &msg)
+{
+    if (LogSettings::get().logToFile) {
+        file_log()->trace(msg);
+        file_log()->flush();
+    }
+
+    if (LogSettings::get().logToStdout) {
+        stdout_log()->trace(msg);
+    }
+
+    if (LogSettings::get().logLast) {
+        last_log()->trace(msg);
+        last_log()->flush();
+    }
+}
+
+void CGL::Logger::debug(const std::string &msg)
+{
+    if (LogSettings::get().logToFile) {
+        file_log()->debug(msg);
+        file_log()->flush();
+    }
+
+    if (LogSettings::get().logToStdout) {
+        stdout_log()->debug(msg);
+    }
+
+    if (LogSettings::get().logLast) {
+        last_log()->debug(msg);
+        last_log()->flush();
+    }
+}
+
+void CGL::Logger::info(const std::string &msg)
+{
+    if (LogSettings::get().logToFile) {
+        file_log()->info(msg);
+        file_log()->flush();
+    }
+
+    if (LogSettings::get().logToStdout) {
+        stdout_log()->info(msg);
+    }
+
+    if (LogSettings::get().logLast) {
+        last_log()->info(msg);
+        last_log()->flush();
+    }
+}
+
+void CGL::Logger::warning(const std::string &msg)
+{
+    if (LogSettings::get().logToFile) {
+        file_log()->warn(msg);
+        file_log()->flush();
+    }
+
+    if (LogSettings::get().logToStdout) {
+        stderr_log()->warn(msg);
+    }
+
+    if (LogSettings::get().logLast) {
+        last_log()->warn(msg);
+        last_log()->flush();
+    }
+}
+
+void CGL::Logger::error(const std::string &msg)
+{
+    if (LogSettings::get().logToFile) {
+        file_log()->error(msg);
+        file_log()->flush();
+    }
+
+    if (LogSettings::get().logToStdout) {
+        stderr_log()->error(msg);
+    }
+
+    if (LogSettings::get().logLast) {
+        last_log()->error(msg);
+        last_log()->flush();
+    }
+}
+
+
+
+
---
+++ src/logger.h
@@ -0,0 +1,77 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+
+#include <memory>
+#include <sstream>
+
+#define quick_mtrace(m) CGL::Logger::trace(CGL::cat("[", __FILE__, ":", __LINE__, " ", __FUNCTION__, "] ", m))
+#define quick_trace()   CGL::Logger::trace(CGL::cat("[", __FILE__, ":", __LINE__, " ", __FUNCTION__, "]"))
+
+namespace spdlog{
+class logger;
+}
+
+namespace CGL {
+
+//!
+//! \brief The Logger class
+//! Журналирование.
+//!
+//! Этот класс является обёрткой для spdlog.
+//! Тут реализовано четыре типа журналирования:
+//! - дозапись в единый файл журнала
+//! - запись в файла журнала для последнего запуска, это файл перезаписыватся
+//! - стандартный поток вывода
+//! - поток вывода ошибок
+//!
+//! При чём _стандартный поток вывода_ или _стандартный поток вывода_
+//! выбирается в зависимости от типа сообщения и при влючённой настройке
+//! \link CGL::LogSettings::logToStdout \link
+//!
+class Logger
+{
+    static std::shared_ptr<spdlog::logger> &file_log();
+    static std::shared_ptr<spdlog::logger> &last_log();
+    static std::shared_ptr<spdlog::logger> &stdout_log();
+    static std::shared_ptr<spdlog::logger> &stderr_log();
+
+public:
+
+    static void trace(const std::string &msg);
+    static void debug(const std::string &msg);
+    static void info(const std::string &msg);
+    static void warning(const std::string &msg);
+    static void error(const std::string &msg);
+};
+
+inline std::string cat(){
+    return std::stringstream().str();
+}
+
+inline std::string cat(char){
+    return std::stringstream().str();
+}
+
+
+template <class T, class ... Args>
+auto cat(T&& cur, Args&& ... args){
+    std::stringstream str;
+    str << cur << cat(std::forward<Args>(args)...);
+    return str.str();
+}
+
+template <class T, class ... Args>
+auto cat(char sep, T&& cur, Args&& ... args){
+    std::stringstream str;
+    str << cur << sep << cat(sep, std::forward<Args>(args)...);
+    return str.str();
+}
+
+template <class ... Args>
+std::string makeStr(Args&& ... args){
+    return cat(' ', std::forward<Args>(args)...);
+}
+
+}
+
+#endif // LOGGER_H
