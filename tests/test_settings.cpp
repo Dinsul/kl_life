003c0642ce4c165591df141e5762baf73e9e434f
Dinsul <dinsul2.0@gmail.com>
2019-06-14 15:17:04 +0300

    Исправления в тесте настроек

--- tests/test_settings.cpp
+++ tests/test_settings.cpp
@@ -13,9 +13,9 @@ BOOST_AUTO_TEST_SUITE(test_suite_settings)
 
 BOOST_AUTO_TEST_CASE(test_save_load)
 {
-    auto defaultFilename = cgl::cat(getenv("HOME"), "/.", PRJ_NAME, "/settings.ini");
+    std::string iniFilename = "settings.ini";
 
-    boost::filesystem::path iniPath{defaultFilename};
+    boost::filesystem::path iniPath{iniFilename};
 
     if (boost::filesystem::exists(iniPath))
     {
@@ -24,10 +24,11 @@ BOOST_AUTO_TEST_CASE(test_save_load)
 
     BOOST_CHECK(!boost::filesystem::exists(iniPath));
 
-    // Здесь файл поумолчанью существовать не должен
-    // Будет соответствующее сообщение
     auto &ls = cgl::Settings::get();
 
+    // Здесь файл настроек существовать не должен
+    // Будет соответствующее сообщение
+    ls.load(iniFilename);
 
     // Проверяем значения поумолчанию
     BOOST_CHECK(ls.universeHeight == 200);
@@ -46,7 +47,7 @@ BOOST_AUTO_TEST_CASE(test_save_load)
     // Возвращаем значение поумолчанию
     // Здесь файл поумолчанью всё ещё отсутствует
     // Будет соответствующее сообщение
-    ls.load();
+    ls.load(iniFilename);
 
     BOOST_CHECK(ls.universeHeight == 200);
     BOOST_CHECK(ls.universeWidth == 200);
@@ -56,12 +57,12 @@ BOOST_AUTO_TEST_CASE(test_save_load)
     ls.universeHeight = 30;
     ls.universeWidth = 30;
     ls.generationPeriod = 200;
-    ls.save();
+    ls.save(iniFilename);
 
     // Файл настроек должен был создасться после сохранения
     BOOST_CHECK(boost::filesystem::exists(iniPath));
 
-    ls.load();
+    ls.load(iniFilename);
 
     BOOST_CHECK(ls.universeHeight == 30);
     BOOST_CHECK(ls.universeWidth == 30);
